8b05b0cf2cae9dd80026e589b49a207fe0af88df
dede ardiansya <dedeariansya1@gmail.com>
2021-03-30 05:24:54 +0800

    update 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <h1>404</h1>
+  <div class="py-5 text-center">
+    <h1 class="display-4 font-weight-bold">404</h1>
+    <router-link :to="{ name: 'Home' }" class="btn btn-primary">BACK TO HOME PAGE</router-link>
   </div>
 </template>
